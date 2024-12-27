c2fd2ca3bb87e27f91b46cd772d72d3a730a12ce
Adem berke Nargül <anargul@k1m20s09.42istanbul.com.tr>
2024-12-27 15:32:27 +0300

    ex03 done

---
+++ Python-0/ex03/NULL_not_found.py
@@ -0,0 +1,17 @@
+def printStr(object, str):
+    print(f'{str}: {object} {type(object)}')
+
+def NULL_not_found(object: any) -> int:
+    if object is None:
+        printStr(object, 'Nothing')
+    elif type(object) is float:
+        printStr(object, 'Cheese')
+    elif type(object) is int:
+        printStr(object, 'Zero')
+    elif type(object) is str and len(object) == 0:
+        printStr(object, 'Empty')
+    elif type(object) is bool:
+        printStr(object, 'Fake')
+    else:
+        print('Type not Found')
+    return (1)
