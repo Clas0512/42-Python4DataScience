e4a45d7d838e0d10e4d4f3c09f31f7c3667c41c8
Adem berke Nargül <anargul@k1m04s02.42istanbul.com.tr>
2025-01-06 19:20:23 +0300

    null control fixed

--- Python-0/ex03/NULL_not_found.py
+++ Python-0/ex03/NULL_not_found.py
@@ -4,13 +4,13 @@ def printStr(object, str):
 def NULL_not_found(object: any) -> int:
     if object is None:
         printStr(object, 'Nothing')
-    elif type(object) is float:
+    elif type(object) is float and object != object:
         printStr(object, 'Cheese')
-    elif type(object) is int:
+    elif type(object) is int and object == 0:
         printStr(object, 'Zero')
     elif type(object) is str and len(object) == 0:
         printStr(object, 'Empty')
-    elif type(object) is bool:
+    elif type(object) is bool and object == False:
         printStr(object, 'Fake')
     else:
         print('Type not Found')
