b30754c63840b7e6ef425d5f8ecb5e5197726661
Adem berke Nargül <anargul@k1m04s02.42istanbul.com.tr>
2025-01-06 19:36:19 +0300

    fixed __doc__

--- Python-0/ex06/ft_filter.py
+++ Python-0/ex06/ft_filter.py
@@ -1,26 +1,10 @@
 def ft_filter(func: callable, array: iter):
     """
-    Custom filter function that applies a given function to each element in
-    the iterable
-    and yields only the elements that satisfy the function's condition.
+    filter(function or None, iterable) --> filter object
 
-    Args:
-        func (callable): A function that returns a boolean value (True or
-        False) to filter elements.
-        array (iter): An iterable (e.g., list, tuple, etc.) that contains the
-        elements to be filtered.
-
-    Yields:
-        Elements from the iterable for which the function returns True.
-        If no function is provided, it filters out elements considered as
-        False (e.g., empty strings, 0, etc.).
-
-    Example:
-        >>> list(ft_filter(lambda x: x > 2, [1, 2, 3, 4]))
-        [3, 4]
-
-        >>> list(ft_filter(None, [0, 1, 2, '', 'hello']))
-        [1, 2, 'hello']
+    Return an iterator yielding those items of iterable for which
+    function(item) is true. If function is None, return the items
+    that are true.
     """
     if func:
         yield from [x for x in array if func(x)]
